df72a7cb25c56ee494024437a99fdcb24d0cd0b3
Fitim Maxhuni <fmaxhuni57@gmail.com>
2022-01-02 00:37:07 +0100

    Expense Transaction | GlobalState, AppReducer

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("ON SUBMIT", text, amount);
+    const newTransaction = {
+      id: Math.floor(Math.random() * 10000000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+    setText("");
+    setAmount(0);
+  };
+
   return (
     <>
       <h3>Add new transaction</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input
--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  console.log(amounts);
+  let income = 0;
+  let expense = 0;
+
+  amounts.map((amount) =>
+    amount > 0 ? (income += amount) : (expense -= amount)
+  );
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +$0.00
+          +${income.toFixed(2)}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          -$0.00
+          -${expense.toFixed(2)}
         </p>
       </div>
     </div>
